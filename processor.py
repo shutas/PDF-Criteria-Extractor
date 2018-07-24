c467d9ebe435ec908e5c1d854cc33213cf8fd666
しゅーたす <shutas@umich.edu>
2018-07-24 20:20:41 +0900

    MOD: Changed Output Directory

--- processor.py
+++ processor.py
@@ -2,6 +2,11 @@ import os
 from config import *
 
 def main():
+    # Delete all files in current output directory
+    file_list = [file for file in os.listdir(OUTPUT2_DIR) if file.endswith(".txt")]
+    for file in file_list:
+        os.remove(os.path.join(OUTPUT2_DIR, file))
+    
     # List that we will eventually write to file
     master_list = []
     
@@ -11,23 +16,23 @@ def main():
         print("Starting", file)
         # Read each line
         input_fp = open(os.path.join(OUTPUT1_DIR, file), "rb")
-        line = input_fp.readline()
+        line = input_fp.readline().decode("utf-8")
 
         while line:
             # Omit blank lines
             if line.isspace():
-                line = input_fp.readline()
+                line = input_fp.readline().decode("utf-8")
                 continue
 
             # Append to list
             master_list.append(line.strip())
 
             # Read next line
-            line = input_fp.readline()
+            line = input_fp.readline().decode("utf-8")
 
         # Print list to file
-        output_fp = open(os.path.join(OUTPUT2_DIR, file), "wb+")
-        output_fp.write(str(master_list).encode("utf-8"))
+        output_fp = open(os.path.join(OUTPUT2_DIR, file), "w+")
+        output_fp.write(str(master_list))
 
         input_fp.close()
         output_fp.close()
